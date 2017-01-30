8d0f1762823d58e954e1589bb5383f2398f21099
Jami Laamanen <jamilaamanen@gmail.com>
2017-01-30 12:51:59 +0200

    Added user lists

--- UDPClient/MainWindow.xaml.cs
+++ UDPClient/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
@@ -27,11 +28,15 @@ namespace UDPClient
         EndPoint epServer;
         // data stream
         private byte[] dataStream = new byte[1024];
+        // list of users on channel
+        ObservableCollection<string> users = new ObservableCollection<string>();
 
         public MainWindow()
         {
             InitializeComponent();
             buttonSend.IsEnabled = false;
+            chatBox.TextChanged += chatBox_TextChanged;
+            listBoxUsers.ItemsSource = users;
         }
 
 
@@ -90,6 +95,17 @@ namespace UDPClient
 
                 this.dataStream = new byte[1024];
 
+                sendData = new CustomPacket();
+                sendData.strName = null;
+                sendData.strMessage = null;
+                sendData.cmdCommand = Command.List;
+
+                data = sendData.ToByte();
+
+                clientSocket.BeginSendTo(data, 0, data.Length, SocketFlags.None, epServer, new AsyncCallback(this.SendData), null);
+
+                this.dataStream = new byte[1024];
+
                 // Begin listening for broadcasts
                 clientSocket.BeginReceiveFrom(this.dataStream, 0, this.dataStream.Length, SocketFlags.None, ref epServer, new AsyncCallback(this.ReceiveData), null);
                 buttonSend.IsEnabled = true;
@@ -130,7 +146,9 @@ namespace UDPClient
 
                 this.Dispatcher.Invoke(() =>
                 {
-                    if (receivedData.strMessage != null)
+                    if (receivedData.cmdCommand == Command.List) UpdateList(receivedData.strMessage);
+
+                    if (receivedData.strMessage != null && receivedData.cmdCommand != Command.List)
                         DisplayMessage(receivedData.strMessage + Environment.NewLine);
                 });
 
@@ -150,12 +168,51 @@ namespace UDPClient
         }
 
         /// <summary>
+        /// Scrolls the chatBox to bottom
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void chatBox_TextChanged(object sender, EventArgs e)
+        {
+            chatBox.ScrollToEnd();
+        }
+
+        /// <summary>
         /// Displays the message in the chatBox
         /// </summary>
         /// <param name="messge"></param>
         private void DisplayMessage(string message)
         {
             chatBox.AppendText(message + Environment.NewLine);
+        }
+
+
+        /// <summary>
+        /// Parses the list-message and updates the list
+        /// </summary>
+        /// <param name="message">message to be parsed</param>
+        private void UpdateList(string message)
+        {
+            char[] separators = { '*' };
+
+            string[] substrings = message.Split(separators);
+
+            // add new users to users
+            foreach (string newuser in substrings)
+            {
+                if (!String.IsNullOrEmpty(newuser))
+                {
+                    // if user already exists in users, dont add
+                    if (!users.Any(user => user == newuser)) users.Add(newuser);
+                }
+            }
+            
+            // removes old users from the list
+            for (int i = (users.Count -1); i >= 0; i--)
+            {
+                string oldUser = users[i];
+                if (!substrings.Contains(oldUser)) users.Remove(oldUser);
+            }
         }
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
@@ -170,6 +227,15 @@ namespace UDPClient
                     sendData.strMessage = null;
 
                     byte[] byteData = sendData.ToByte();
+
+                    this.clientSocket.SendTo(byteData, 0, byteData.Length, SocketFlags.None, epServer);
+
+                    sendData = new CustomPacket();
+                    sendData.cmdCommand = Command.List;
+                    sendData.strName = null;
+                    sendData.strMessage = null;
+
+                    byteData = sendData.ToByte();
 
                     this.clientSocket.SendTo(byteData, 0, byteData.Length, SocketFlags.None, epServer);
 
